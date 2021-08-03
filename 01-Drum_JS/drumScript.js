406ab625425d8086b2e2dbbc058137a900fc63d8
jmininel <ju.mininel@hotmail.com>
2021-08-03 19:44:35 -0300

    created pages

--- 01-Drum_JS/drumScript.js
+++ 01-Drum_JS/drumScript.js
@@ -13,7 +13,6 @@ function playSound(e) {
   audio.play();
 }
 
-
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
